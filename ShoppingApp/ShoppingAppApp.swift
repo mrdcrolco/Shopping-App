b35cbb0d03c2b59d32cb8d9269f836fb01ab86db
Andrey <apiddubnij21@gmail.com>
2024-01-14 09:23:30 -0500

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Andrii Piddubnyi on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
