82133d7fdd8ae1d0c063d5e296f8c4d0abc44bbd
Andrey <apiddubnij21@gmail.com>
2024-01-14 11:53:43 -0500

    Created a new file and made LoginView

---
+++ ShoppingApp/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  ShoppingApp
+//
+//  Created by Andrii Piddubnyi on 1/14/24.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
